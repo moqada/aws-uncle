4544f2a39c55f79a6120b419c9a9c0939e71599a
moqada <moqada@gmail.com>
2015-10-12 22:32:19 +0900

    feat(job): Remove job process
    - AWS Lambda support time-based task
    
    BREAKING CHANGE: Remove job script

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
 
-import Job from './Job';
 import Uncle from './Uncle';
-import {env} from './utils';
 import configs from './configs.json';
 
 dotenv.load();
@@ -16,12 +14,6 @@ dotenv.load();
  */
 export function handler(event, context) {
   console.log('env: ', process.env);
-  const job = new Job(env('JOBCTL_BUCKET_NAME'), env('JOBCTL_KEY_NAME'), {
-    execInterval: env('NOTIFY_INTERVAL'),
-    jobInterval: env('JOB_INTERVAL')
-  });
-  job.execute(event, context, ctx => {
-    const uncle = new Uncle(configs);
-    uncle.scold().then(() => ctx.done(null, 'done'));
-  });
+  const uncle = new Uncle(configs);
+  uncle.scold().then(() => context.done(null, 'done'));
 }
