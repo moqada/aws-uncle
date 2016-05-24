7e5318e795e8aed0c2cae480a2fb781275aca5c9
moqada <moqada@gmail.com>
2016-05-24 23:41:29 +0900

    refactor(eslint): Update for eslint-config-moqada 15.0.0

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import dotenv from 'dotenv';
 
 import Uncle from './Uncle';
+
+/* eslint-disable node/no-unpublished-import, node/no-missing-import */
 import configs from './configs.json';
+
+/* eslint-enable */
 
 dotenv.load();
 
